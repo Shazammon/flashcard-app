34bfd3729baf6bed25ae5ae22bfc134fe457d2aa
Shazammon <ammonorgill@gmail.com>
2023-02-18 15:28:36 -0500

    set up sample fashcards variable

--- src/App.js
+++ src/App.js
@@ -8,4 +8,29 @@ function App() {
   );
 }
 
+const SAMPLE_FLASHCARDS = [
+  {
+    id: 1,
+    question: "What is 2 + 2?",
+    answer: '4',
+    options: [
+      '2',
+      '3',
+      '4',
+      '5'
+    ]
+  },
+  {
+    id: 2,
+  question: "Question 2",
+    answer: '4',
+    options: [
+      '2',
+      '3',
+      '4',
+      '5'
+    ]
+  },
+]
+
 export default App;

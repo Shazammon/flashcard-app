98bf78437348ff6209d990ac93f404fe696f0aef
Shazammon <ammonorgill@gmail.com>
2023-02-18 15:32:56 -0500

    updated flashcard list component

--- src/FlashcardList.js
+++ src/FlashcardList.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 export default function FlashcardList({ flashcards }) {
   return (
-    <div>
+    <div className='card-grid'>
+        {flashcards.map(flashcard => {
+            return <Flashcard flashcard={flashcard} key={flashcard.id} />
+        })}
       
     </div>
   )

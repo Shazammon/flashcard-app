f49d6687d126614ca37604ebe9bfb034f21ebead
Shazammon <ammonorgill@gmail.com>
2023-02-22 16:41:11 -0700

    set up flashcard option in div

--- src/Flashcard.js
+++ src/Flashcard.js
@@ -13,7 +13,7 @@ export default function Flashcard({flashcard}) {
         {flashcard.question}
         <div className="flashcard">
           {flashcard.options.map(option => {
-            return <div className='flashcard-option'></div>
+            return <div className='flashcard-option'>{option}</div>
           })}
         </div>
       </div>

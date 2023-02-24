a1dab9b839c7c2ef4dd7e3e2858b9df00896e7ff
Shazammon <ammonorgill@gmail.com>
2023-02-24 13:56:57 -0700

    finished app

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import axios from 'axios'
 
 function App() {
 
-  const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
+  const [flashcards, setFlashcards] = useState([])
   const [categories, setCategories] = useState([])
 
   const categoryEl = useRef()
+  const amountEl = useRef()
 
   useEffect(() => {
     axios
@@ -45,6 +46,26 @@ function App() {
 
   function handleSubmit(e) {
     e.preventDefault()
+    axios
+    .get(`https://opentdb.com/api.php`, {
+      params: {
+        amount: amountEl.current.value,
+        category: categoryEl.current.value
+      }
+    })
+    .then(res => {
+      setFlashcards(res.data.results.map((questionItem, index) => {
+        const answer = decodeString(questionItem.correct_answer)
+        const options = [...questionItem.incorrect_answers.map(a => decodeString(a)), answer]
+        
+        return {
+          id: `${index}-${Date.now()}`,
+          question: decodeString(questionItem.question),
+          answer: answer,
+          options: options.sort(() => Math.random() - .5)
+        }
+      }))
+    })
 
   }
   return (
@@ -54,10 +75,18 @@ function App() {
           <label htmlFor="category">Category</label>
           <select id="category" ref={categoryEl}>
             {categories.map(category => {
-              return <option ></option>
+              return <option value={category.id} key={category.id}>{category.name}</option>
             })}
-
           </select>
+
+        </div>
+        <div className="form-group">
+          <label htmlFor="amount">Number of Questions</label>
+          <input type="number" id="amount" min="1" step="1" defaultValue={10} ref={amountEl} />
+        </div>
+        
+        <div className="form-group">
+            <button className="btn">Generate</button>
 
         </div>
 
@@ -70,29 +99,6 @@ function App() {
   );
 }
 
-const SAMPLE_FLASHCARDS = [
-  {
-    id: 1,
-    question: "What is 2 + 3?",
-    answer: '4',
-    options: [
-      '2',
-      '3',
-      '4',
-      '5'
-    ]
-  },
-  {
-    id: 2,
-  question: "Question 2",
-    answer: 'Answer',
-    options: [
-      '2',
-      '3',
-      '4',
-      '5'
-    ]
-  },
-]
+
 
 export default App;

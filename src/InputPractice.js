8e0055b7b72c75878124639dc1990388ba059ad7
Shazammon <ammonorgill@gmail.com>
2023-02-28 13:45:51 -0700

    set up AbortController

--- src/InputPractice.js
+++ src/InputPractice.js
@@ -1,17 +1,18 @@
 import { useState, useEffect } from 'react'
 
-export function useFetch() {
+export function useFetch(url) {
     const [loading, setLoading] = useState(true)
     const [data, setData] = useState()
     const [error, setError] = useState()
 
     useEffect(() => {
+        const controller = new AbortController()
         setLoading(true)
-        fetch(url)
+        fetch(url, { signal: controller.signal })
         .then(setData)
         .catch(setError)
         .finally(()=> setLoading(false))
-    })
+    }, [url])
 
     return { loading, data, error }
 }

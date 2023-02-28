0e56a37f063ed526ec215416f5fe4c26036f651c
Shazammon <ammonorgill@gmail.com>
2023-02-28 13:43:26 -0700

    set up state for loading, data, and error

--- src/InputPractice.js
+++ src/InputPractice.js
@@ -1,8 +1,13 @@
 import { useState, useEffect } from 'react'
 
 export function useFetch() {
+    const [loading, setLoading] = useState(true)
+    const [data, setData] = useState()
+    const [error, setError] = useState()
 
+    useEffect(() => {
 
+    })
 
     return { loading, data, error }
 }
